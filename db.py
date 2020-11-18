fc715633d0fd7e7e48c8458d6719021a42487df1
Takoi <fvdelows@mail.ru>
2020-11-18 23:35:05 +0300

    rewrite db and reload sqldatabase

--- db.py
+++ db.py
@@ -7,24 +7,22 @@ class Admin(db.Model):
     status = db.Column(db.Integer, unique=False, nullable=False, default='0')
 
 
-class Photo(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    descr = db.Column(db.String(80), unique=True, nullable=False)
-    filename = db.Column(db.String(80), unique=False, nullable=False)
-
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
     descr = db.Column(db.String(80), unique=False, nullable=False)
 
-class Files(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(80), unique=False, nullable=False)
 
-class News(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    text_filename = db.Column(db.String(80), unique=False, nullable=False)
-    image_filename = db.Column(db.String(80), unique=False, nullable=False)
 
+class Universities(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    add_score = db.Column(db.String(80), unique=True, nullable=False)
+
+
+class Faculties(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    university = db.Column(db.String(80), unique=False, nullable=False)
+    subjects = db.Column(db.String(80), unique=False, nullable=False)
+    passing_score = db.Column(db.Integer, unique=False, nullable=False)
 
 db.create_all()
