52e104ee3916cd2efd2157e947e2f4ba1aaa9764
Takoi <fvdelows@mail.ru>
2020-11-18 23:04:13 +0300

    wrong diviser - *

--- little_functions.py
+++ little_functions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-divider = "1"
+divider = ""
 def information_extractor(name):
     text = open("static/text_data/" + name[:-4] + divider + name[-4:], "r").read()
     return text.split("\n/*/\n")
