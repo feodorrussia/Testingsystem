cb65693bf32f1f07d54198d251cf6cd8ff74029c
Takoi <fvdelows@mail.ru>
2020-11-29 21:15:42 +0300

    **

--- little_functions.py
+++ little_functions.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 divider = ""
-def information_extractor(name):
+def information_extractor_f(name):
     text = open("static/text_data/" + name[:-4] + divider + name[-4:], "r").read()
     return text.split("\n/*/\n")
