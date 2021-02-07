edda9f8ce207a5dc1df2202ceac551e556a259b5
Takoi <fvdelows@mail.ru>
2021-02-07 21:23:45 +0300

    изменение БД под новую архететуру

--- db.py
+++ db.py
@@ -13,18 +13,43 @@ class Data(db.Model):
     descr = db.Column(db.String(1200), unique=False, nullable=False)
 
 
-
-class Universities(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    add_score_fl = db.Column(db.String(80), unique=True, nullable=False)
-
-
 class Faculties(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(240), unique=True, nullable=False)
-    university = db.Column(db.String(120), unique=False, nullable=False)
+    id_uni = db.Column(db.Integer, unique=False, nullable=False)
     subjects = db.Column(db.String(80), unique=False, nullable=False)
-    limit_scores = db.Column(db.String(80), unique=False, nullable=False)
     passing_score = db.Column(db.Integer, unique=False, nullable=False)
+
+
+class Universities(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(120), unique=True, nullable=False)
+
+
+class Individual_achivements(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+    descr = db.Column(db.String(1280), unique=False, nullable=False)
+
+
+class University_Ach(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_uni = db.Column(db.Integer, unique=False, nullable=False)
+    id_ach = db.Column(db.Integer, unique=False, nullable=False)
+    point = db.Column(db.Integer, unique=False, nullable=False)
+    descr = db.Column(db.String(320), unique=False, nullable=False)
+
+
+class Subjects(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
+
+
+class University_Sub(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_uni = db.Column(db.Integer, unique=False, nullable=False)
+    id_sub = db.Column(db.Integer, unique=False, nullable=False)
+    limit_score = db.Column(db.Integer, unique=False, nullable=False)
+
 
 db.create_all()
