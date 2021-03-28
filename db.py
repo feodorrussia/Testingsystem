cc05bd41871800a2242368e18376937a3cc7c44d
Takoi <fvdelows@mail.ru>
2021-03-28 22:01:07 +0300

    добавил запись комбинаций предметов в БД

--- db.py
+++ db.py
@@ -10,7 +10,7 @@ class Admin(db.Model):
 class Data(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
-    descr = db.Column(db.String(1200), unique=False, nullable=False)
+    descr = db.Column(db.String(1200), unique=False, nullable=True)
 
 
 class Faculties(db.Model):
@@ -29,7 +29,7 @@ class Universities(db.Model):
 class Individual_achivements(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=False, nullable=False)
-    descr = db.Column(db.String(1280), unique=False, nullable=False)
+    descr = db.Column(db.String(1280), unique=False, nullable=True)
 
 
 class University_Ach(db.Model):
@@ -37,7 +37,7 @@ class University_Ach(db.Model):
     id_uni = db.Column(db.Integer, unique=False, nullable=False)
     id_ach = db.Column(db.Integer, unique=False, nullable=False)
     point = db.Column(db.Integer, unique=False, nullable=False)
-    descr = db.Column(db.String(320), unique=False, nullable=False)
+    descr = db.Column(db.String(320), unique=False, nullable=True)
 
 
 class Subjects(db.Model):
@@ -50,6 +50,13 @@ class University_Sub(db.Model):
     id_uni = db.Column(db.Integer, unique=False, nullable=False)
     id_sub = db.Column(db.Integer, unique=False, nullable=False)
     limit_score = db.Column(db.Integer, unique=False, nullable=False)
+
+class Sub_Comb(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_fac = db.Column(db.Integer, unique=False, nullable=False)
+    subs = db.Column(db.String(80), unique=False, nullable=False)
+    user = db.Column(db.Integer, unique=False, nullable=False)
+    fl = db.Column(db.Integer, unique=False, nullable=False)
 
 
 db.create_all()
--- little_functions.py
+++ little_functions.py
@@ -4,3 +4,4 @@ def information_extractor_f(name):
     text = open("static/text_data/" + name[:-4] + divider + name[-4:], "r").read()
     return text.split("\n/*/\n")
 
+sub_combs = {}
